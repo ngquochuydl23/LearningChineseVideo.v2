d167daae4bcaf3b7830aec2d9235b3c9af68087c
unknown <nguyenquochuydl123@gmail.com>
2024-08-19 11:07:17 +0700

    [api] add production database

--- api/LearningVideoApi.v2/src/models/index.js
+++ api/LearningVideoApi.v2/src/models/index.js
@@ -6,14 +6,15 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config.json');
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.NODE_ENV === 'development') {
+  sequelize = new Sequelize(config.database, config.username, config.password, config.development);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config.production);
 }
 
 fs
