7073379e5b2c1b41f7c847d1fc83c7e64ee912c2
unknown <nguyenquochuydl123@gmail.com>
2024-09-09 09:46:43 +0700

    [web] Finish stage 1

--- web/LearningChineseVideo/src/sections/saved/saved-voca-video.js
+++ web/LearningChineseVideo/src/sections/saved/saved-voca-video.js
@@ -7,50 +7,23 @@ const SavedVocaVideo = ({ video, lastUpdated, savedCount }) => {
     return (
         <Link
             style={{ textDecoration: 'none' }}
-            href={"/saved/" + video.id}>
+            href={"/saved/" + video._id}>
             <Stack
                 direction="row"
                 sx={{ width: '100%', textDecoration: 'none' }}>
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-end',
-                        alignItems: 'flex-end',
-                        position: 'relative'
-                    }}>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end', position: 'relative' }}>
                     <img
-                        style={{
-                            borderRadius: '10px',
-                            height: '100px',
-                            width: '170px',
-                            transition: 'transform .2s',
-                            position: 'relative'
-                        }}
+                        style={{ borderRadius: '10px', height: '100px', width: '170px', transition: 'transform .2s', position: 'relative' }}
                         src={readMediaUrl(video.thumbnail)}
                     />
-                    <div
-                        style={{
-                            margin: '10px',
-                            display: 'flex',
-                            position: 'absolute',
-                            zIndex: 1
-                        }}>
+                    <div style={{ margin: '10px', display: 'flex', position: 'absolute', zIndex: 1 }}>
                         <Chip
-                            sx={{
-                                fontWeight: '600',
-                                backgroundColor: 'green',
-                                fontSize: '14px',
-                                color: 'white'
-                            }}
+                            sx={{ fontWeight: '600', backgroundColor: 'green', fontSize: '14px', color: 'white' }}
                             label={"HSK " + video.level} />
                     </div>
                 </div>
                 <Box sx={{ padding: '10px', ml: '10px', width: "100%" }}>
-                    <Stack
-                        justifyContent="space-between"
-                        direction="row"
-                        sx={{ width: "100%" }}>
+                    <Stack justifyContent="space-between" direction="row" sx={{ width: "100%" }}>
                         <Typography
                             fontSize="20px"
                             gutterBottom
@@ -60,16 +33,10 @@ const SavedVocaVideo = ({ video, lastUpdated, savedCount }) => {
                             {video.title}
                         </Typography>
                     </Stack>
-                    <Typography
-                        fontSize='14px'
-                        variant="subtitle2"
-                        color="text.secondary">
+                    <Typography fontSize='14px' variant="subtitle2" color="text.secondary">
                         Từ vựng lưu gần nhât: {moment(lastUpdated).format("DD/MM/YYYY")}
                     </Typography>
-                    <Typography
-                        fontSize='14px'
-                        variant="subtitle2"
-                        color="text.secondary">
+                    <Typography fontSize='14px' variant="subtitle2" color="text.secondary">
                         Số từ vựng đã lưu: {savedCount}
                     </Typography>
                 </Box>
