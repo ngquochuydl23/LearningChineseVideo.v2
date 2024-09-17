e98b6d08db285478337476b7bc1e3de8e44ea8ad
unknown <nguyenquochuydl123@gmail.com>
2024-09-17 13:25:31 +0700

    [api-v2] add LikeController

--- api/LearningVideoApi.v2/src/controllers/likeController.js
+++ api/LearningVideoApi.v2/src/controllers/likeController.js
@@ -4,13 +4,14 @@ const _ = require('lodash');
 const commentModel = require('../nosql-models/comment.model');
 const toObjectId = require('../utils/toObjectId');
 const videoModel = require('../nosql-models/video.model');
+const likeModel = require('../nosql-models/like.model');
 
 
-exports.getLikesByVideoId = async (req, res, next) => {
+exports.getLikedVideos = async (req, res, next) => {
     const loggingUserId = req.loggingUserId;
 
     try {
-        const comments = await commentModel
+        const likes = await likeModel
             .aggregate([
                 {
                     $match: {
@@ -37,7 +38,6 @@ exports.getLikesByVideoId = async (req, res, next) => {
                         _id: 1,
                         creatorId: 1,
                         videoId: 1,
-                        content: 1,
                         createdAt: 1,
                         owned: 1,
                         'creator._id': 1,
@@ -53,7 +53,60 @@ exports.getLikesByVideoId = async (req, res, next) => {
                 }
             ])
 
-        return httpOk(res, comments);
+        return httpOk(res, likes);
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.getLikesByVideoId = async (req, res, next) => {
+    const loggingUserId = req.loggingUserId;
+
+    try {
+        const likes = await likeModel
+            .aggregate([
+                {
+                    $match: {
+                        creatorId: toObjectId(loggingUserId),
+                        isDeleted: false
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'Video.Collection',
+                        localField: 'videoId',
+                        foreignField: '_id',
+                        as: 'video'
+                    }
+                },
+                { $unwind: '$video' },
+                {
+                    $project: {
+                        _id: 1,
+                        creatorId: 1,
+                        videoId: 1,
+                        createdAt: 1,
+                        owned: 1,
+                        'video._id': 1,
+                        'video.title': 1,
+                        'video.description': 1,
+                        'video.thumbnail': 1,
+                        'video.mimeType': 1,
+                        'video.level': 1,
+                        'video.topics': 1,
+                        'video.viewerCount': 1,
+                        'video.commentCount': 1,
+                        'video.likeCount': 1,
+                    }
+                },
+                {
+                    $sort: {
+                        createdAt: -1,
+                    }
+                }
+            ])
+
+        return httpOk(res, likes);
     } catch (error) {
         next(error);
     }
@@ -61,11 +114,7 @@ exports.getLikesByVideoId = async (req, res, next) => {
 
 exports.addLike = async (req, res, next) => {
     const loggingUserId = req.loggingUserId;
-    const {
-        commentId,
-        content,
-        videoId,
-    } = req.body;
+    const { videoId } = req.body;
     try {
 
         const video = await videoModel.findById(videoId);
@@ -73,18 +122,27 @@ exports.addLike = async (req, res, next) => {
             throw new AppException("Video not found");
         }
 
-        const comment = new commentModel({
-            content,
+        var like = await likeModel.findOne({
             creatorId: toObjectId(loggingUserId),
             videoId: toObjectId(videoId)
         });
 
-        await comment.save();
 
-        video.commentCount = video.commentCount + 1;
+        if (like) {
+            throw new AppException("You've already like this video");
+        }
+
+        like = new likeModel({
+            creatorId: toObjectId(loggingUserId),
+            videoId: toObjectId(videoId)
+        });
+
+        await like.save();
+
+        video.likeCount = video.likeCount + 1;
         await video.save();
 
-        return http201(res, comment, 'Created comment successfully');
+        return http201(res, undefined, 'Created like successfully');
     } catch (error) {
         next(error);
     }
@@ -94,84 +152,51 @@ exports.delLike = async (req, res, next) => {
     const loggingUserId = req.loggingUserId;
 
     try {
-        const comment = await commentModel.findOne({
+        const like = await likeModel.findOne({
             creatorId: toObjectId(loggingUserId),
-            _id: toObjectId(req.params.commentId)
+            likeId: toObjectId(req.params.likeId)
         });
 
-        if (!comment) {
-            throw new AppException("Comment not found");
+        if (!like) {
+            throw new AppException("Like not found");
         }
 
-        const video = await videoModel.findById(comment.videoId);
+        const video = await videoModel.findById(like.videoId);
         if (!video) {
-            throw new AppException("Video not found");
+            throw new AppException("Like not found");
         }
 
-        if (video.commentCount > 0) {
-            video.commentCount = video.commentCount - 1;
+        if (video.likeCount > 0) {
+            video.likeCount = video.likeCount - 1;
             await video.save();
         }
 
-        comment.isDeleted = true;
-        await comment.save();
+        like.isDeleted = true;
+        await like.save();
 
-        return httpOk(res, null, 'Deleted comment successfully');
+        return httpOk(res, null, 'Deleted like successfully');
     } catch (error) {
         next(error);
     }
 }
 
-exports.getMyLikeVideo = async (req, res, next) => {
+
+exports.getLike = async (req, res, next) => {
     const loggingUserId = req.loggingUserId;
 
     try {
+        const like = await likeModel.findOne({
+            creatorId: toObjectId(loggingUserId),
+            videoId: toObjectId(req.params.videoId)
+        });
 
-        const documents = await commentModel
-            .aggregate([
-                {
-                    $match: {
-                        _id: toObjectId(req.params.commentId),
-                        isDeleted: false
-                    }
-                },
-                {
-                    $lookup: {
-                        from: 'User.Collection',
-                        localField: 'creatorId',
-                        foreignField: '_id',
-                        as: 'creator'
-                    }
-                },
-                { $unwind: '$creator' },
-                {
-                    $addFields: {
-                        owned: { $eq: ['$creatorId', toObjectId(loggingUserId)] }
-                    }
-                },
-                {
-                    $project: {
-                        _id: 1,
-                        creatorId: 1,
-                        videoId: 1,
-                        content: 1,
-                        createdAt: 1,
-                        owned: 1,
-                        'creator._id': 1,
-                        'creator.fullName': 1,
-                        'creator.avatar': 1,
-                        'creator.gender': 1,
-                    }
-                }
-            ])
-
-
-        if (_.isEmpty(documents)) {
-            throw new AppException("Comment not found");
+        if (!like) {
+            throw new AppException("Like not found");
         }
 
-        return httpOk(res, documents[0]);
+        return httpOk(res, like);
     } catch (error) {
         next(error);
     }
 }
+
--- api/LearningVideoApi.v2/src/routes/likeRoute.js
+++ api/LearningVideoApi.v2/src/routes/likeRoute.js
@@ -4,9 +4,9 @@ const likeController = require('../controllers/likeController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
 router.get('/by-video/:videoId', authMiddleware, likeController.getLikesByVideoId);
+router.get('/by-video/liked-video', authMiddleware, likeController.getLikedVideos);
 router.post('/', authMiddleware, likeController.addLike);
-router.get('/my-like-video', authMiddleware, likeController.getMyLikeVideo)
-router.delete('/:commentId', authMiddleware, likeController.delLike)
-
+router.delete('/:likeId', authMiddleware, likeController.delLike)
+router.get('/by-video/:videoId/check', authMiddleware, likeController.getLike);
 
 module.exports = router;
