aa5197450de7c7cabffbe399c78f79e992e3c71d
Thai Bao <boishiro2002@gmail.com>
2024-09-29 14:45:53 +0700

    like, comment website 29/09 (BAO)

--- web/LearningChineseVideo/src/pages/videos/[id].js
+++ web/LearningChineseVideo/src/pages/videos/[id].js
@@ -1,17 +1,17 @@
-import Head from 'next/head';
 import {
-    Box,
-    Stack,
-    Unstable_Grid2 as Grid,
+  Box,
+  Unstable_Grid2 as Grid,
+  Stack,
 } from '@mui/material';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 import _ from 'lodash';
-import { useEffect, useState } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import VideoPlayer from 'src/sections/video/video-player';
+import { useEffect, useState } from 'react';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 import VideoInfo from 'src/sections/video/video-info';
+import VideoPlayer from 'src/sections/video/video-player';
 import { getVideo, viewVideo } from 'src/services/api/video-api';
-
 const ProductDetailPage = () => {
 
     const [anchorEl, setAnchorEl] = useState(null);
@@ -50,17 +50,21 @@ const ProductDetailPage = () => {
                     videoUrl={video.videoUrl}
                     subtitles={video.subtitles}
                 />
-                <Stack
+                {/* ở đây */}
+                
+                <Stack 
                     display="flex"
                     direction="row">
                     <VideoInfo
                         {...video}
                         tags={video.topics} />
+                       
                     <Stack
                         flex="1"
                         display="flex">
                     </Stack>
                 </Stack>
+                
             </Box >
         </>
     )
